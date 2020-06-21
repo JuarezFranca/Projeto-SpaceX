d5c4f72aa5da6398d15e7a8a450f3b7bedfe52b3
Juarez França <kjootaa@gmail.com>
2020-06-20 22:40:47 -0300

    puxando informações da API

--- pages/foguete.js
+++ pages/foguete.js
@@ -1,14 +1,25 @@
 import Link from 'next/link'
+import axios from 'axios'
 
-export default function foguete() {
+export default function foguete({dados}) {
     return (
         <>
-            <h1>Foguete</h1>
+            <div>
+                <h1>Foguete</h1>
+                {console.log(dados)}
+            </div>
             <h2>
+
                 <Link href="/index">
                     <a>retorne inicio</a>
                 </Link>
             </h2>
         </>
     )
+    foguete.getInitialProps = async () => {
+        const response = await axios.get(
+            'https://api.spacexdata.com/v3/rockets'
+        )
+        return { dados: response.first_stage }
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,29 @@
-import react from "react"
+import react from 'react'
 import Link from 'next/link'
+import axios from 'axios'
 
 
 export default function Home() {
     return (
         <>
             <h1>SpaceX</h1>
-            <li>
-                <ul>
-                    <Link href="/foguete">
-                        <a>Lista de Foguetes</a>
-                    </Link>
-                </ul>
-            </li>
-            <li>
-                <ul>
-                    <Link href="/capsulas">
-                        <a>Capsulas</a>
-                    </Link>
-                </ul>
-            </li>
-            <li>
-                <ul>
-                    <Link href="/historico">
-                        <a>Historico</a>
-                    </Link>
-                </ul>
-            </li>
+            <div>
+                <Link href="/foguete">
+                    <a>Lista de Foguetes</a>
+                </Link>
+            </div>
+
+            <div>
+                <Link href="/capsulas">
+                    <a>Capsulas</a>
+                </Link>
+            </div>
+
+            <div>
+                <Link href="/historico">
+                    <a>Historico</a>
+                </Link>
+            </div>
 
         </>
     )
